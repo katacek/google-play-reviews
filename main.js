0abe6b47ba45d8451779aa76ef56ccf0cd57b2d6
Kateřina Hroníková <56041262+katacek@users.noreply.github.com>
2022-05-31 10:24:29 +0200

    Fix broken selectors

--- main.js
+++ main.js
@@ -1,146 +1,74 @@
-
 const Apify = require('apify');
 
-Apify.main(async () =>
-{
+Apify.main(async () => {
+    const input = await Apify.getInput();
 
-    try
+    if (input.appUrl == undefined && input.appId == undefined)
     {
-   
-        const input = await Apify.getValue('INPUT');
-        // const input =
-        // {
-        //     "appUrl": "https://play.google.com/store/apps/details?id=com.nick.noggin&hl=en_US&gl=US",
-        //     "appId": "com.nick.noggin",
-        //     "limit": 10,
-        //     "sort": 'Newest'
-        // };
+        throw (new Error('input.appUrl or input.appId must be provided'))
+    }
+
+    let url = '';
+
+    if (input.appUrl)
+    {
+        url = `${input.appUrl}&showAllReviews=true`;
+    }
+    else
+    {
+        url = `https://play.google.com/store/apps/details?id=${input.appId}&showAllReviews=true`;
+    }
+    //const appId = input.appId;
+    const limit = input.limit;
+
+    if (input.limit > 4000)
+    {
+        throw (new Error('Maximum reviews limit is set to 4000.'))
+    }
 
 
-        if (input.appUrl == undefined && input.appId == undefined)
+    console.log('Launching Puppeteer...');
+    const proxyConfiguration = await Apify.createProxyConfiguration({groups:['BUYPROXIES94952']});
+    const browser = await Apify.launchPuppeteer({
+        proxyUrl:proxyConfiguration.newUrl()
+    });
+
+    console.log(`Opening page ${url}...`);
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.evaluate(function () {
+        Array.from(document.querySelectorAll('button'))
+    .find(el => el.textContent === 'See all reviews').click();});
+    let count=(await page.$$('div.RHo1pe')).length;
+    while(count<limit)
+    {
+        await Apify.utils.puppeteer.infiniteScroll(page,{timeoutSecs:5});
+        let newCount = (await page.$$('div.RHo1pe')).length;
+        if(count==newCount)
         {
-            throw (new Error('input.appUrl or input.appId must be provided'))
-        }
-
-        let url = '';
-   
-        if (input.appUrl)
-        {
-            url = `${input.appUrl}&showAllReviews=true`;
+            console.log('All reviews found '+count);
+            break;
         }
         else
         {
-            url = `https://play.google.com/store/apps/details?id=${input.appId}&showAllReviews=true`;
+            count = newCount;
         }
-        //const appId = input.appId;
-        const limit = input.limit;
-    
-        if (input.limit > 4000)
-        {
-            throw (new Error('Maximum reviews limit is set to 4000.'))
-        }
-
-        const browser = await Apify.launchPuppeteer({
-            launchOptions: {
-                headless: false,
-            }
-        });
-   
-        console.log(`Opening URL: ${url}`);
-        const page = await browser.newPage();
-        
-        await page.setBypassCSP(true);
-        await page.goto(url);
-        
-        console.log('Bypass CSP has been set up')
-
-        await Apify.utils.puppeteer.injectJQuery(page);
-        if (input.sort)
-        {
-            await page.click('div[jscontroller="iDykod"]');
-            await page.waitFor(1000);
-            await page.keyboard.type(input.sort);
-            await page.waitFor(1000);
-            await page.keyboard.press('Enter');
-            await page.waitFor(3000);
-        }
-        
-        let numberOfReviews = await page.evaluate(x => ($('div[jscontroller][jsdata][jsmodel]').not('[jsaction]').get().length));
-        if (numberOfReviews < limit)
-        {
-            // timeoutSecs: If 0, will scroll until bottom of page.
-            // waitForSecs: How many seconds to wait for no new content to load before exit
-            await Apify.utils.puppeteer.infiniteScroll(page, { timeoutSecs: 0, waitForSecs: 10 });
-            numberOfReviews = await page.evaluate(x => ($('div[jscontroller][jsdata][jsmodel]').not('[jsaction]').get().length));
-            if (numberOfReviews < limit)
-            {
-
-                while ((await page.evaluate(x => $('span:contains(Show More)[JSSLOT]').text())) != null && numberOfReviews <= limit)
-                {
-                    await page.evaluate(x => $('span:contains(Show More)[JSSLOT]').click());
-                    await Apify.utils.puppeteer.infiniteScroll(page, { timeoutSecs: 0, waitForSecs: 10 });
-                    numberOfReviews = await page.evaluate(x => ($('div[jscontroller][jsdata][jsmodel]').not('[jsaction]').get().length));
-                }
-            }
-        }
-
-        console.log('Downloading reviews...');
-        let reviews = await page.evaluate(x =>
-        {
-            let reviews = [];
-
-            reviewDivs = $('div[jscontroller][jsdata][jsmodel]').not('[jsaction]');
-            reviewDivs.each(function (index)
-            {
-                review = {}
-                const spans = $('span', this).map(function ()
-                {
-                    return $(this).text();
-                });
-
-                // shift() Remove the first item of an array
-                const rating = $('div[aria-label*=Rated]', this).attr('aria-label').match(/\d+/).shift();
-                const text = $('span[jsname]', this).map(function ()
-                {
-                    return $(this).text();
-                });
- 
-                review.name = spans[0];
-                review.date = spans[2];
-                review.rating = rating;
-                review.text = text[text.length - 1];
-                if (review.text == '')
-                {
-                    review.text = text[text.length - 2];
-                }
-                reviews.push(review);
-            })
-            return reviews;
-        })
-
-        if (reviews.length > limit)
-            reviews = reviews.slice(0, limit);
- 
-        // Save output
-        //const appNameHash = appId.replace(/\./g, '-')
-    
-        //await Apify.setValue(`Google-play-reviews-${appNameHash}`, reviews);
-
-        //const dataset = await Apify.openDataset(`Google-play-reviews-${appNameHash}`);
-        //await dataset.pushData(reviews);
-    
-        await Apify.pushData(reviews);
-           
-        console.log('Data saved..')
-    
     }
-
-    catch (err)
+    const reviews = await page.$$('div.RHo1pe');
+    for(const review of reviews.slice(0,input.limit))
     {
+        const result = {};
+        result.name = await review.$eval('header div.YNR7H div.X5PpBb', (el)=>el.textContent);
+        result.date = await review.$eval('.bp9Aid', (el)=>el.textContent);
+        result.rating = (await review.$$('span.Z1Dz7b')).length;
+        result.text = await review.$eval('.h3YV2d', (el)=>el.textContent);
+        await Apify.pushData(result);
+    }
+  
+  
 
-        console.log(err)
-        throw (err)
-   
-    };
+    console.log('Closing Puppeteer...');
+    await browser.close();
 
+    console.log('Done.');
 });
